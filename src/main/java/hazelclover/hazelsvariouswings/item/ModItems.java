13bd36a63375e41400f06585db9e6fd66ee21b0d
Joey <74479165+Naradoxy@users.noreply.github.com>
2024-09-16 02:28:42 -0300

    basic useless item

---
+++ src/main/java/hazelclover/hazelsvariouswings/item/ModItems.java
@@ -0,0 +1,29 @@
+package hazelclover.hazelsvariouswings.item;
+
+import hazelclover.hazelsvariouswings.HazelsVariousWings;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item FLIMSY_WINGS = registerItem("flimsy_wings", new Item(new Item.Settings()));
+
+    private static void addItemsToToolsTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(FLIMSY_WINGS);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(HazelsVariousWings.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        HazelsVariousWings.LOGGER.info("Registering Mod Items for " + HazelsVariousWings.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModItems::addItemsToToolsTabItemGroup);
+    }
+}
